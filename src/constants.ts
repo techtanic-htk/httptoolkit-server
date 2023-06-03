184c8bbc9c826c0c44657eb65aa6e2f653f7bcca
techtanic <techtanic@outlook.com>
2023-06-03 18:45:26 +0530

    .

--- src/constants.ts
+++ src/constants.ts
@@ -12,6 +12,7 @@ export const ALLOWED_ORIGINS = IS_PROD_BUILD
         // ensures that no other sites/apps can communicate with your server
         // whilst you have the app open. If they could (requires an HTTP mitm),
         // they would be able to start proxies & interceptors.
+        /^https:\/\/app\.httptoolkit\.tech$/,
         /^https:\/\/techtanic-htk\.github\.io$/
     ]
     : [
@@ -21,6 +22,7 @@ export const ALLOWED_ORIGINS = IS_PROD_BUILD
         // resolving to localhost and never hitting the network).
         /^https?:\/\/localhost(:\d+)?$/,
         /^http:\/\/local\.httptoolkit\.tech(:\d+)?$/,
+        /^https:\/\/app\.httptoolkit\.tech$/,
         /^https:\/\/techtanic-htk\.github\.io$/,
     ];
 
